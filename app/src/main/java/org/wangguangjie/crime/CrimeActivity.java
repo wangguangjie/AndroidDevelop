f124270659a5684cec876a317bf63b14eaeff9d9
光杰 <1414490417@qq.com>
2017-10-11 21:26:27 +0800

    "update"

---
+++ app/src/main/java/org/wangguangjie/crime/CrimeActivity.java
@@ -0,0 +1,10 @@
+package org.wangguangjie.crime;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by wangguangjie on 2017/10/11.
+ */
+
+public class CrimeActivity extends FragmentActivity {
+}
---
+++ app/src/main/java/org/wangguangjie/crime/CrimeListActivity.java
@@ -0,0 +1,8 @@
+package org.wangguangjie.crime;
+
+/**
+ * Created by wangguangjie on 2017/10/11.
+ */
+
+public class CrimeListActivity {
+}
